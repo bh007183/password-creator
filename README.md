5641078c65fa09f9e5b58132f53f2cf5f2588abb
Ben Hopkins <bjhops17@gmail.com>
2020-12-09 18:03:13 -0800

    Javascript code linked with html.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-creator
+This application is for generating a password at random based on input you provide. The minimum requirment that the user must adhere to in creating their password is to provide a number between 8 and 128 (inclusive). Any number over or under will not work and you will recieve an error message in the generation box. 
+The application uses html, css, and javascript.
--- script.js
+++ script.js
@@ -1,25 +1,74 @@
 
-//Main function
-function generatePassword() {
-  return "password"
-}
-
-  // How long should password be?
+//   Main function
+function generatePassword(){
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numeric = ["0","1","2","3","4","5","6","7","8","9"];
+  var specialChar = ["!","@","#","$","%","^","&","*","+","="];
+  var compile = [];
   
+  // User interface variables w/ "if" statement checking for validity of inpute.
+  
+  var numberGiven = prompt("How long would you like the password? It must be between 8 and 128 (inclusive).");
+  
+  if (numberGiven >= 8 && numberGiven <= 128){
+    
+  var questionLower = confirm("Would you like lower case letters in your password? If YES, click OK, if NO, click cancel");
+  var questionSpecial = confirm("Would you like special charectors in your password? If YES, click OK, if NO, click cancel");
+  var questionUpper = confirm("Would you like upper case letters in your password? If YES, click OK, if NO, click cancel");
+  var questionNumeric = confirm("Would you like numbers in your password? If YES, click OK, if NO, click cancel");
+  
+  //Loop for checking criteria requested by user and adding those characters requested to empty array titled "compile".
+  
+  while(compile.length < numberGiven){
 
-
+    if (questionLower === true){
+        var lower = Math.floor(Math.random() * lowerCase.length);
+        var lowerPass = lowerCase[lower]
+        if (compile.length < numberGiven){         
+            compile.push(lowerPass);
+        }
+      }
+            
+        
+    if (questionUpper === true){
+        var upper = Math.floor(Math.random() * upperCase.length);
+        var upperPass = upperCase[upper];
+        if (compile.length < numberGiven){         
+            compile.push(upperPass);
+        }
+      }
+        
+      
+    if (questionNumeric === true){
+        var number = Math.floor(Math.random() * numeric.length);
+        var numberPass = numeric[number];
+        if (compile.length < numberGiven){         
+            compile.push(numberPass);
+        }
+      }   
+    
+    if (questionSpecial === true){
+        var special = Math.floor(Math.random() * specialChar.length);
+        var specialPass = specialChar[special];
+        if (compile.length < numberGiven){         
+            compile.push(specialPass);
+        }
+      }
+    }
+  
+    // New variable with combined "compile" array.
+     
+     var password = compile.join("");  
+    //password returned to screen.
    
+     return password
+   
+    // Invalid Entry message to screen.
+  } else {return "Invalid Entry. Try Again."}
+}
+        
 
-//TODO: When button clicked, series of promps.
-//TODO: Select which criteria to include in the password when prompted.
-//TODO: 1st criteria prompt will be how long should the password be. must be between 8 and 128 characters. 
-//TODO: If fails, alert message.
-//TODO: Ask if user wants lowercase.
-//TODO: Ask if user wants uppercase.
-//TODO: Ask if user wants numeric.
-//TODO: And/or ask if user wants special characters.
-// at least one charector that user selects from each catigory must be used.
-//TODO:generat password matching above specifications.
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
